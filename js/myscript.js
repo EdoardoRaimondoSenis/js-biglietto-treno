db995aa881590e4ebb33b5cd8373218a650e9dbd
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-06-04 18:28:09 +0200

    correzione

--- js/myscript.js
+++ js/myscript.js
@@ -3,13 +3,11 @@ let anni = parseInt(prompt("Inserire l'età"));
 let prezzobiglietto
 
 if (anni <= 18) {
-    prezzobiglietto = anni * ((km * 0.21) * 20 / 100);
+    prezzobiglietto = (km * 0.21) * 20 / 100;
 } else (anni > 18 && anni < 65); {
-    prezzobiglietto = anni * (km * 0.21);
-}
-
-if (anni >= 65) {
-    prezzobiglietto = anni * ((km * 0.21) * 40 / 100);
+    prezzobiglietto = km * 0.21;
+} if (anni >= 65) {
+    prezzobiglietto = gm (km * 0.21) * 40 / 100;
 }
 
 console.log(prezzobiglietto)
